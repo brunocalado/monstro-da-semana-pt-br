e2f02359d036f5da144fc8d116ff4758b8f3d91c
Bruno Calado <brunocalado@users.noreply.github.com>
2024-02-12 12:42:56 -0300

    0.2.7

--- scripts/motw-for-pbta.mjs
+++ scripts/motw-for-pbta.mjs
@@ -21,9 +21,38 @@ Hooks.once('init', () => {
 Hooks.once('pbtaSheetConfig', () => {
 
    // Disable the sheet config form.
-   game.settings.set('pbta', 'sheetConfigOverride', true);
+   //game.settings.set('pbta', 'sheetConfigOverride', true);
 
    // Replace the game.pbta.sheetConfig with the MotW version.
-   configSheet();
+   //configSheet();
 
+	game.pbta.tagConfigOverride = {
+	  item: {
+		// Tags available to all actors
+      all: '[ {"value":"1-dano"},{"value":"2-dano"},{"value":"3-dano"}, {"value":"4-dano"}, {"value":"área"}, {"value":"automática"}, {"value":"balanceada"}, {"value":"barreira"}, {"value":"barulhenta"}, {"value":"brutal"}, {"value":"contato"}, {"value":"drena-vida"}, {"value":"empurrão"}, {"value":"ignora-armadura"}, {"value":"incendiária"}, {"value":"inconveniente"}, {"value":"lenta"}, {"value":"longe"}, {"value":"mágica"}, {"value":"numerosa"}, {"value":"pequena"}, {"value":"perto"}, {"value":"pesada"}, {"value":"pessoal"}, {"value":"rápida"}, {"value":"recarga"}, {"value":"restritiva"}, {"value":"sagrada"}, {"value":"útil"}, {"value":"valiosa"}, {"value":"volátil"}, {"value":"[material]"} ]'
+	  }
+	}
+   
+/* EXAMPLE
+game.pbta.tagConfigOverride = {
+  // Tags available to any actor and item
+  general: '[{"value":"fire"}, {"value": "water"}, {"value": "earth"}, {"value": "air"}]',
+  actor: {
+    // Tags available to all actors
+    all: '[{"value":"person"}]',
+    // Tags available to a specific actor type set up on game.pbta.sheetConfig.actorTypes (e.g. "character", "npc")
+    character: '[{"value":"mook"}]'
+  },
+  item: {
+    // Tags available to all actors
+    all: '[{"value":"consumable"}]',
+    // Tags available to a specific item type (e.g. "equipment", "move")
+    move: '[{"value":"sword"}]'
+  }
+}
+
+*/   
+   
+  
+  
 });
